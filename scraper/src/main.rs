2bda5e90302bff9f2099e3dd56f5413b3428ae9f
Casper Meijn <casper@meijn.net>
2024-06-17 16:59:38 +0200

    fix: Send user agent to make sure the request is accepted

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -15,26 +15,35 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    handle("https://www.rijksoverheid.nl/onderwerpen/grondwet-en-statuut/vraag-en-antwoord/wanneer-kan-ik-de-vlag-uithangen-en-wat-is-de-vlaginstructie", "article", "data/rijksoverheid.md");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    handle("https://www.rijksoverheid.nl/onderwerpen/grondwet-en-statuut/vraag-en-antwoord/wanneer-kan-ik-de-vlag-uithangen-en-wat-is-de-vlaginstructie", "article", "data/rijksoverheid.md").await;
     handle(
         "https://www.koninklijkhuis.nl/onderwerpen/vlaggen-en-vlaginstructie/vlaginstructie",
         "article",
         "data/koninklijkhuis.md",
-    );
+    )
+    .await;
 
     Ok(())
 }
 
-fn handle(url: &str, class_id: &str, filename: &str) {
-    let contents = scrape_page(url, class_id).unwrap();
-
-    let text = format!("# Url\n{}\n\n{}", url, contents);
-    write_or_compare_file(text, filename);
+async fn handle(url: &str, class_id: &str, filename: &str) {
+    match scrape_page(url, class_id).await {
+        Ok(contents) => {
+            let text = format!("# Url\n{}\n\n{}", url, contents);
+            write_or_compare_file(text, filename);
+        }
+        Err(err) => eprintln!("error: {}", err),
+    }
 }
 
-fn scrape_page(url: &str, class_id: &str) -> Result<String, reqwest::Error> {
-    let resp = reqwest::blocking::get(url)?.text()?;
+async fn scrape_page(url: &str, class_id: &str) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .user_agent("VlaginstructieScraper/0.1")
+        .build()?;
+
+    let resp = client.get(url).send().await?.text().await?;
 
     let document = select::document::Document::from(resp.as_str());
 
